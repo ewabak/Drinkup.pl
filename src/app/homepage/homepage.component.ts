ac021e07b985104a13e2a5aec834751637289f97
Maciej Dobosz <dobosz.ma@interia.pl>
2018-05-07 22:33:57 +0200

    Testy AJAXa

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,4 +12,35 @@ export class HomepageComponent implements OnInit {
   ngOnInit() {
   }
 
+  fetchDrinks() {
+    var scope = this;
+
+    var HTTPRequest = new XMLHttpRequest();
+    HTTPRequest.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    HTTPRequest.onload = function() {
+      var drinksArray = JSON.parse(HTTPRequest.responseText);
+      scope.printDrinksToView(drinksArray);
+    };
+    HTTPRequest.send();
+  }
+
+  printDrinksToView(drinksArray) {
+    var outputDrinks = document.getElementById('drinks_list');
+
+    drinksArray['drinks'].forEach((drink) => {
+      var outputHTML = '<div class="drink">' +
+                          '<img src="' + drink['strDrinkThumb'] + '">' +
+                          '<div>' +
+                            '<h1>' +
+                            drink['strDrink']  +
+                            '</h1>' +
+                            '<h2>' +
+                            drink['strCategory'] +
+                            '</h2>' +
+                          '</div>' +
+                        '</div>';
+      outputDrinks.innerHTML += outputHTML;
+      console.log(drink);
+    });
+  }
 }
